a51548e3487826697fa60c583f9af0a1410a76fb
pc <whgustjr302@kaist.ac.kr>
2020-05-28 23:09:15 +0900

    middle

--- MyGL.cpp
+++ MyGL.cpp
@@ -57,80 +57,42 @@ bool MyGL::RasterizeTriangle(GLVertex verts[3]) {
 	//
 
 	// Implement rasterization here. You are NOT required to implement wireframe mode (i.e. 'w' key mode).
-	double edge01[3],  edge12[3], edge20[3];
 	double edge[3][3];
-	GLVertex& v0 = verts[0];
-	GLVertex& v1 = verts[1];
-	GLVertex& v2 = verts[2];
 	for (int k = 0; k < 3; k++) {
 		edge[k][0] = verts[k].position[1] - verts[(k + 1) % 3].position[1];
 		edge[k][1] = verts[k].position[0] - verts[(k + 1) % 3].position[0];
 		edge[k][2] = verts[k].position[0] * verts[(k + 1) % 3].position[1] - verts[(k + 1) % 3].position[0] * verts[k].position[1];
 	}
-	edge01[0] = v0.position[1] - v1.position[1];	
-	edge01[1] = v0.position[0] - v1.position[0];	
-	edge01[2] = v0.position[0] * v1.position[1] - v1.position[0] * v0.position[1];	
-	edge12[0] = v1.position[1] - v2.position[1];	
-	edge12[1] = v1.position[0] - v2.position[0];	
-	edge12[2] = v1.position[0] * v2.position[1] - v2.position[0] * v1.position[1];
-	edge20[0] = v2.position[1] - v0.position[1];	
-	edge20[1] = v2.position[0] - v0.position[0];	
-	edge20[2] = v2.position[0] * v0.position[1] - v0.position[0] * v2.position[1];
-	double area = 0.5 * (edge01[2] + edge12[2] + edge20[2]);
-	glm::vec4 color0;
-	glm::vec4 color1;
-	glm::vec4 color2;
-
-	color0 = v0.color;
-	color1 = v1.color;
-	color2 = v2.color;
-
-	double Arred = 1 / (2 * area) * (color0[0] * edge01[0] + color1[0] * edge12[0] + color2[0] * edge20[0]);
-	double Brred = 1 / (2 * area) * (color0[0] * edge01[1] + color1[0] * edge12[1] + color2[0] * edge20[1]);
-	double Crred = 1 / (2 * area) * (color0[0] * edge01[2] + color1[0] * edge12[2] + color2[0] * edge20[2]);
-	double Arblue = 1 / (2 * area) * (color0[1] * edge01[0] + color1[1] * edge12[0] + color2[1] * edge20[0]);
-	double Brblue = 1 / (2 * area) * (color0[1] * edge01[1] + color1[1] * edge12[1] + color2[1] * edge20[1]);
-	double Crblue = 1 / (2 * area) * (color0[1] * edge01[2] + color1[1] * edge12[2] + color2[1] * edge20[2]);
-	double Argreen = 1 / (2 * area) * (color0[2] * edge01[0] + color1[2] * edge12[0] + color2[2] * edge20[0]);
-	double Brgreen = 1 / (2 * area) * (color0[2] * edge01[1] + color1[2] * edge12[1] + color2[2] * edge20[1]);
-	double Crgreen = 1 / (2 * area) * (color0[2] * edge01[2] + color1[2] * edge12[2] + color2[2] * edge20[2]);
-	double z0 = 1 / (2 * area) * (v0.position[2] * edge01[0] + v1.position[2] * edge12[0] + v2.position[2] * edge20[0]);
-	double z1 = 1 / (2 * area) * (v0.position[2] * edge01[1] + v1.position[2] * edge12[1] + v2.position[2] * edge20[1]);
-	double z2 = 1 / (2 * area) * (v0.position[2] * edge01[2] + v1.position[2] * edge12[2] + v2.position[2] * edge20[2]);
-
-	double xmax = std::max(v0.position[0], v1.position[0], v2.position[0]);
-	double xmin = std::min(v0.position[0], v1.position[0], v2.position[0]);
-	double ymax = std::max(v0.position[1], v1.position[1], v2.position[1]);
-	double ymin = std::min(v0.position[1], v1.position[1], v2.position[1]);
-
+	double area = 0.5 * (edge[0][2] + edge[1][2] + edge[2][2]);
+	double rgb[3][3];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			rgb[i][j] = 1 / (2 * area) * (verts[0].color[i] * edge[0][j] + verts[1].color[i] * edge[1][j] + verts[2].color[i] * edge[2][j]);
+		}
+	}
+	double depth[3];
+	for (int i = 0; i < 3; i++) {
+		depth[i] = 1 / (2 * area) * (verts[0].position[2] * edge[0][i] + verts[1].position[2] * edge[1][i] + verts[2].position[2] * edge[2][i]);
+	}
+	double xmax = std::max(verts[0].position[0], std::max(verts[1].position[0], verts[2].position[0]));
+	double xmin = std::min(verts[0].position[0], std::max(verts[1].position[0], verts[2].position[0]));
+	double ymax = std::max(verts[0].position[1], std::max(verts[1].position[1], verts[2].position[1]));
+	double ymin = std::min(verts[0].position[1], std::max(verts[1].position[1], verts[2].position[1]));
 	for (int x = xmin; x <= xmax; x++) {
-		for (int y = ymin; y <= xmax; x++) {
+		for (int y = ymin; y <= ymax; y++) {
+			double Exy[3];
+			for (int i = 0; i < 3; i++) {
+				Exy[i] = edge[i][0] * x + edge[i][1] * y + edge[i][2];
+			}
 			bool t[3];
 			for (int j = 0; j < 3; j++) {
-
+				if (edge[j][0] != 0) {
+					t[j] = (edge[j][0] > 0);
+				}
+				else {
+					t[j] = (edge[j][1] > 0);
+				}
 			}
-			if (edge01[0] != 0) {
-				t[0] = (edge01[0] > 0); 
-			}
-			else {
-				t[0] = (edge01[1] > 0);
-			}
-			if (edge01[0] != 0) {
-				t[1] = (edge12[0] > 0);
-			}
-			else {
-				t[1] = (edge12[1] > 0);
-			}
-			if (edge01[0] != 0) {
-				t[2] = (edge20[0] > 0); 
-			}
-			else {
-				t[2] = (edge20[1] > 0);
-			}
-			double Exy[3];
-			Exy[0] = edge01[0] * x + edge01[1] * y + edge01[2];
-			Exy[1] = edge12[0] * x + edge12[1] * y + edge12[2];
-			Exy[2] = edge20[0] * x + edge20[1] * y + edge20[2];
 			int triangle = 0;
 			for (int i = 0; i < 3; i++) {
 				if ((Exy[i] > 0) || t[i]) {
@@ -148,17 +110,17 @@ bool MyGL::RasterizeTriangle(GLVertex verts[3]) {
 					}
 				}
 				else {
-					color[0] = Arred * x + Brred * y + Crred;
-					color[1] = Arblue * x + Brblue * y + Crblue;
-					color[2] = Argreen * x + Brgreen * y + Crgreen;
+					for (int i = 0; i < 3; i++) {
+						color[i] = rgb[i][0] * x + rgb[i][1] * y + rgb[i][2];
+					}
 				}
-
 				if (depthTestEnabled) {
-					double z = z0 * x + z1 * y + z2;
+					double z = depth[0] * x + depth[1] * y + depth[2];
 					if (z < frameBuffer.GetDepth(x, y)) {
 						frameBuffer.SetDepth(x, y, z);
 						frameBuffer.SetPixel(x, y, color);
 					}
+					
 				}
 				else {
 					frameBuffer.SetPixel(x, y, color);
